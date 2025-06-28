f128d9d850295fd9bde87bb233301a88d9f7c321
SkyzofrnDev <dirgahardekaagustiantara@gmail.com>
2025-06-29 00:32:57 +0700

    Navbar

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+import { Navbar } from "./Components/Index";
+function App() {
+  return (
+    <div className="bg-[#1A1B1F] h-screen w-full">
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
